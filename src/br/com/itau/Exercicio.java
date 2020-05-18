48c5507c7a66de37c645a7eaf5045cef11cead41
Andre Firmo <andrefirmo@outlook.com>
2020-05-18 09:23:42 -0300

    desafio OK

--- src/br/com/itau/Exercicio.java
+++ src/br/com/itau/Exercicio.java
@@ -1,9 +1,13 @@
 package br.com.itau;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import br.com.itau.modelo.Lancamento;
 import br.com.itau.service.LancamentoService;
+
 
 public class Exercicio {
 
@@ -12,6 +16,36 @@ public class Exercicio {
 		
 		//TODO: continue daqui
 		System.out.println("Hello");
+
+
+		// Listagem de gastos ordenados por mes
+		lancamentos.stream().sorted(Comparator.comparing(Lancamento::getMes))
+				.collect(Collectors.toList())
+				.forEach(list -> System.out.println(list));
+
+		//listagem de mes
+
+		lancamentos.stream()
+				.filter( c -> c.getMes().equals(1) )
+				.sorted( Comparator.comparing(Lancamento::getMes))
+				.collect(Collectors.toList() )
+				.forEach( l -> System.out.println( "lancamentos do mes " + l ) );
+		//soma da listagem do mes
+		double soma = lancamentos.stream()
+				.filter( c -> c.getMes().equals(1) )
+				.mapToDouble(Lancamento::getValor)
+				.sum();
+		System.out.println("Soma do mes "+soma);
+
+		// listagem de categoria
+		lancamentos.stream()
+				.filter( c -> c.getCategoria().equals(1) )
+				.sorted( Comparator.comparing(Lancamento::getMes))
+				.collect(Collectors.toList() )
+				.forEach( l -> System.out.println( "categoria " + l ) );
+
+
 	}
+
 
 }
